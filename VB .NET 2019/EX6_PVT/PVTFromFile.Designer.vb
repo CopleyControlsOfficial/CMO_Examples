f1bfc599b4d32274ad92418f5f531562804211f3
Copley Controls <117777616+CopleyControlsOfficial@users.noreply.github.com>
2025-01-14 18:31:35 -0500

    Delete VB .NET 2019 directory

--- VB .NET 2019/EX6_PVT/PVTFromFile.Designer.vb
+++
@@ -1,7 +0,0 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class PVTFromFile
-    Inherits System.Windows.Forms.Form
-
-    Friend WithEvents Label1 As Label
-
-End Class
